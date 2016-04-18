f9684da60e7cfeaf84eacbcf7ace126fb171e28a
Tyler Kivari <tyler@checkout51.com>
2016-04-18 13:59:54 -0400

    testing for touch handler

--- assets/js/highlighter.js
+++ assets/js/highlighter.js
@@ -214,7 +214,7 @@
 			console.log("1: " + distance_1);
   			var distance_2 = Math.sqrt(Math.pow(this.finger_1_end.x - this.finger_2_end.x, 2) + Math.pow(this.finger_2_end.y - this.finger_2_end.y, 2));
   			console.log("2: " + distance_2);
-  			scale *= distance_2 / distance_1;
+  			scale = distance_2 / distance_1;
   			console.log(scale);
 		}
 		
