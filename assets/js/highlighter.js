001c4a3ff928b0b3d18c29f064eb2af9328f5904
Tyler Kivari <tyler@checkout51.com>
2016-04-18 14:59:20 -0400

    testing for touch handler

--- assets/js/highlighter.js
+++ assets/js/highlighter.js
@@ -217,7 +217,7 @@
   			if (distance_1 && distance_2) {
 	  			console.log("2: " + distance_2);
 	  			this.ratio = distance_2 / distance_1;
-	  			context.scale(this.ratio, this.ratio);
+	  			context.scale(this.ratio/2, this.ratio/2);
 	  			console.log("ratio: " + this.ratio);
 	  			scale *= this.ratio;
   			}
