dd7344ef67d789238ae9ca342b27f27fb5158d2c
Tyler Kivari <tyler@checkout51.com>
2016-04-19 11:33:41 -0400

    focus on origin between finger points when pinch zooming

--- assets/js/highlighter.js
+++ assets/js/highlighter.js
@@ -219,16 +219,15 @@
   			if (distance_1 && distance_2) {
 	  			this.ratio = (distance_2 / distance_1);
 	  			
-	  			originx = highlighter.origin.x/(scale*this.ratio) - highlighter.origin.x/scale;
-	  			originy = highlighter.origin.y/(scale*this.ratio) - highlighter.origin.y/scale;
-	  			
 	  			if (highlighter.origin) {
 	  				console.log(highlighter.origin);
-	  				context.translate(originx, originy)
+	  				context.translate(highlighter.origin.x, highlighter.origin.y)
 	  			}
 	  			
 	  			context.scale(this.ratio, this.ratio);
-	  			
+	  			originx = highlighter.origin.x/(scale*this.ratio) - highlighter.origin.x/scale;
+	  			originy = highlighter.origin.y/(scale*this.ratio) - highlighter.origin.y/scale;
+
 	  			if (highlighter.origin) {
 	  				context.translate(-originx, -originy)
 	  			}
