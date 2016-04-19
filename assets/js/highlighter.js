2c51cb2599b6f7b65f26d592f48e5d43229829a0
Tyler Kivari <tyler@checkout51.com>
2016-04-19 13:43:56 -0400

    focus on origin between finger points when pinch zooming

--- assets/js/highlighter.js
+++ assets/js/highlighter.js
@@ -224,21 +224,22 @@
   			if (distance_1 && distance_2) {
 	  			this.ratio = (distance_2 / distance_1);
 	  			
-  			context.translate(highlighter.origin.x, highlighter.origin.y)
-  			
-  			
-  			context.scale(this.ratio, this.ratio);
-  			if (highlighter.origin) {
-	  			originx -= highlighter.origin.x/this.ratio;
-	  			originy -= highlighter.origin.y/this.ratio;
-	  			console.log("zoom: " + this.ratio);
-	    		console.log("x: " + originx);
-	    		console.log("y: " + originy);
-  			}
+	  			context.translate(highlighter.origin.x, highlighter.origin.y)
+	  			
+	  			
+	  			context.scale(this.ratio, this.ratio);
+	  			
+	  			context.translate(-highlighter.origin.x, -highlighter.origin.y)
+	  			
+	  			scale *= this.ratio; // redraws the empty rectangle at proper scaled size to avoid multiple instances of the image on the canvas
 
-  			context.translate(-highlighter.origin.x, -highlighter.origin.y)
-  			
-  			scale *= this.ratio; // redraws the empty rectangle at proper scaled size to avoid multiple instances of the image on the canvas
+	  			if (highlighter.origin) {
+		  			originx -= highlighter.origin.x/scale*this.ratio - highlighter.origin.x/scale;
+		  			originy -= highlighter.origin.y/scale*this.ratio - highlighter.origin.y/scale;
+		  			console.log("zoom: " + this.ratio);
+		    		console.log("x: " + originx);
+		    		console.log("y: " + originy);
+	  			}
   			}
 		}
 		
